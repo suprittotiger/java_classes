fbc2ebfc8671ffb62df67191fcf2a1a8ff088e84
suprittotiger <113026854+suprittotiger@users.noreply.github.com>
2025-03-11 20:41:58 +0530

    Add files via upload

---
+++ two.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class two{
+    public static void main(String args[]){
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("enter the number");
+       int num1=scanner.nextInt();
+       System.out.println("enter the number");
+       int num2=scanner.nextInt();
+       int max=num1;
+       if(max<num2){
+          max=num2;
+       }
+       System.out.println("max of given two number is:"+max);
+    }
+}
